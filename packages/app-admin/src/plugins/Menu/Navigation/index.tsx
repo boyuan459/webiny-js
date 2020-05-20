2d076ad2bade24e4f240be8f2527cda7d1ffcdab
Bo Yuan <boyuan954@gmail.com>
2020-05-20 16:06:11 +1000

    feat: ✨  cleanup menus
    cleanup unnecessary links in menus
    
    BREAKING CHANGE:
    app admin

--- packages/app-admin/src/plugins/Menu/Navigation/index.tsx
+++ packages/app-admin/src/plugins/Menu/Navigation/index.tsx
@@ -1,20 +1,12 @@
 import React, { useMemo, useEffect } from "react";
 import { sortBy } from "lodash";
 import { Drawer, DrawerContent, DrawerHeader } from "@webiny/ui/Drawer";
-import { List, ListItem, ListItemGraphic } from "@webiny/ui/List";
 import { IconButton } from "@webiny/ui/Button";
-import { Icon } from "@webiny/ui/Icon";
 import { getPlugin, getPlugins } from "@webiny/plugins";
 import { HeaderLogoPlugin, MenuPlugin } from "@webiny/app-admin/types";
 import { useNavigation, Menu, Item, Section } from "./components";
-import { logoStyle, MenuFooter, MenuHeader, navContent, navHeader, subFooter } from "./Styled";
+import { logoStyle, MenuHeader, navContent, navHeader } from "./Styled";
 import { ReactComponent as MenuIcon } from "@webiny/app-admin/assets/icons/baseline-menu-24px.svg";
-import { ReactComponent as DocsIcon } from "@webiny/app-admin/assets/icons/icon-documentation.svg";
-import { ReactComponent as CommunityIcon } from "@webiny/app-admin/assets/icons/icon-community.svg";
-import { ReactComponent as GithubIcon } from "@webiny/app-admin/assets/icons/github-brands.svg";
-
-import { i18n } from "@webiny/app/i18n";
-const t = i18n.ns("app-admin/navigation");
 
 const Navigation = () => {
     const { hideMenu, menuIsShown, initSections } = useNavigation();
@@ -52,58 +44,6 @@ const Navigation = () => {
                 </MenuHeader>
             </DrawerHeader>
             <DrawerContent className={navContent}>{menus}</DrawerContent>
-            <MenuFooter>
-                <List nonInteractive>
-                    <a href="https://docs.webiny.com/" rel="noopener noreferrer" target="_blank">
-                        <ListItem ripple={false}>
-                            <ListItemGraphic>
-                                <Icon icon={<DocsIcon />} />
-                            </ListItemGraphic>
-                            {t`Documentation`}
-                        </ListItem>
-                    </a>
-                    <a
-                        href="https://community.webiny.com/"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        <ListItem ripple={false}>
-                            <ListItemGraphic>
-                                <Icon icon={<CommunityIcon />} />
-                            </ListItemGraphic>
-                            {t`Community`}
-                        </ListItem>
-                    </a>
-                    <a
-                        href="https://github.com/webiny/webiny-js"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        <ListItem ripple={false}>
-                            <ListItemGraphic>
-                                <Icon icon={<GithubIcon />} />
-                            </ListItemGraphic>
-                            {t`Source`}
-                        </ListItem>
-                    </a>
-                    <ListItem ripple={false} className={subFooter}>
-                        <div>
-                            <a
-                                href="https://www.webiny.com/"
-                                rel="noopener noreferrer"
-                                target="_blank"
-                            >
-                                {t`Webiny.com`}
-                            </a>
-                            <p>
-                                {t`© {year} Webiny Ltd, London, UK`({
-                                    year: new Date().getFullYear()
-                                })}
-                            </p>
-                        </div>
-                    </ListItem>
-                </List>
-            </MenuFooter>
         </Drawer>
     );
 };
